cfff13ac2218db9853b74a1a982ec62a75ef5955
Habibullah Hamdard <88111023+HabibullahFirdous@users.noreply.github.com>
2024-04-19 19:07:40 +0430

    Update Profile.jsx

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -6,7 +6,7 @@ export default function Profile() {
   return (
     <>
       <div className="profile">
-        <img src="https://unsplash.com/photos/person-in-blue-denim-jeans-and-black-jacket-sitting-on-rock-formation-near-body-of-water-PUKQhn0t8fg" />
+        <img src="https://source.unsplash.com/WLxQvbMyfas" />
         <div className="profile-info">
           <h1 className="user-name"> Laura Smith</h1>
           <p className="user-job">Frontend Developer</p>
